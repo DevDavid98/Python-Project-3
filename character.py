cac7934da1671bbc2751663852cb8e9ae76876bc
OXDavidXO <44665014+OXDavidXO@users.noreply.github.com>
2019-12-29 20:36:05 -0600

    Create character.py
    this is what i have so far.

---
+++ character.py
@@ -0,0 +1,18 @@
+class Character:
+    original = []
+    def __init__(self, char, was_guessed = False):
+        self.char = char
+        self.was_guessed = False
+
+    def guess_letter(self):
+        self.original.append(self.char)
+        
+        if self.char in self.original:
+            self.was_guessed = True
+            return '-' * 25
+        else:
+            self.was_guessed = False
+            
+    def reveal_letter(self):
+        if self.was_guessed == True:
+            return ' '.join(self.original)
