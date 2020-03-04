d77af54ffbe30ae2dc32c209dabcf822fed62274
OXDavidXO <44665014+OXDavidXO@users.noreply.github.com>
2020-03-04 04:16:09 -0600

    Final Character.py update
    refined the code and added pep8 standards

--- character.py
+++ character.py
@@ -1,6 +1,8 @@
 class Character:
     original = []
-    def __init__(self, char, was_guessed = False):
+
+    def __init__(self, char, was_guessed=False):
+
         self.char = char
         self.was_guessed = False
 
@@ -8,14 +10,16 @@ class Character:
         if self.char not in self.original:
             self.original.append(self.char)
             return ''
-        
+
         elif self.char in self.original:
             self.was_guessed = True
+            return ''
 
     def reveal_letter(self):
-        print('Your recently guessed letter: {}'.format(' '.join(self.original)))
-        del self.original[:]
+        print('Your recently guessed letters: {}'
+              .format(' '.join(self.original)))
+
+        if self.was_guessed is True:
+            print('The letter, "{}" is already guessed.'
+                  .format(' '.join(self.char)))
         return ''
-        if self.was_guessed == True:
-            print('')
-            return 'The letter, "{}" is already guessed.'.format(' '.join(self.char))
