2c773c8f90f16abea1504236d62cac43b2036979
David Garcia <44665014+OXDavidXO@users.noreply.github.com>
2020-03-10 03:56:40 -0500

    last update to character.py
    added comments

--- character.py
+++ character.py
@@ -1,11 +1,16 @@
 class Character:
+
+    # stores the original letter guessed to be used in comparison.
     original = []
 
+    # starts the class with two parameters with char storing a letter.
     def __init__(self, char, was_guessed=False):
 
         self.char = char
         self.was_guessed = False
 
+    # this method stores letters in the original list
+    # also checks if letter was guessed
     def guess_letter(self):
         if self.char not in self.original:
             self.original.append(self.char)
@@ -15,6 +20,8 @@ class Character:
             self.was_guessed = True
             return ''
 
+    # This method shows you the letters guessed
+    # also tells the player if the letter was guessed
     def reveal_letter(self):
         print('Your recently guessed letters: {}'
               .format(' '.join(self.original)))
