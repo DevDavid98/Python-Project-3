95bf1c819a2117e08c13b3ffd1f0f3e029c2a5cb
David Garcia <44665014+OXDavidXO@users.noreply.github.com>
2020-03-10 03:58:39 -0500

    Update app.py
    added comments

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+# imports the WHOLE game.py file
 from game import Game
 
+# makes sure the game is the GAME
 if __name__ == "__main__":
     game = Game()
     game()
+
+# Have a wonderful day/night to who ever reads this.
+# Hopefully Everthing checks out
+# I think I fixed the issue that made me fail, hopefully I did.
+# I think I was supposed to make the Phrase class
+# parameter phrase = the list called the_answers
+# To make the code more moduler, or maybe I overthink things hmm..
